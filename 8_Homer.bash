175a1397df97fe4a837f6d235d41e2a8bf5f784d
sarahhainer <43413817+sarahhainer@users.noreply.github.com>
2018-10-03 14:53:16 -0400

    Update 8_Homer.bash

--- 8_Homer.bash
+++ 8_Homer.bash
@@ -13,8 +13,8 @@ annotatePeaks.pl /path_to/peak_file.txt mm10 -size 4000 -hist 20 -d /path_to/CR1
 annotatePeaks.pl /path_to/peak_file.txt mm10 -size 4000 -hist 20 -ghist -d /path_to/CR1_1-120/ > 1_120_heatmap.txt
 annotatePeaks.pl /path_to/peak_file.txt mm10 -size 4000 -hist 20 -ghist -d /path_to/CR1_150-500/ > 150_500_heatmap.txt
 
-findPeaks CR1_1-120/ -style factor -o auto -P 1 -poisson 0.01 -F 0.5 -L 1 -LP 0.001 -i CR1noab_1-120/
-findPeaks CR1_150-500/ -style histone -o auto -P 1 -poisson 0.01 -F 0.5 -L 1 -LP 0.001 -i CR1noab_150-500/
+findPeaks CR1_1-120/ -style factor -o auto -P 1 -poisson 0.01 -F 0.5 -L 2 -LP 0.001 -i CR1noab_1-120/
+findPeaks CR1_150-500/ -style histone -o auto -P 1 -poisson 0.01 -F 0.5 -L 2 -LP 0.001 -i CR1noab_150-500/
 
 annotatePeaks.pl CR1_1-120/peaks.txt mm10 > CR1_1-120/annotatedpeaks.txt
 annotatePeaks.pl CR1_150-500/regions.txt mm10 > CR1_150-500/annotatedregions.txt
